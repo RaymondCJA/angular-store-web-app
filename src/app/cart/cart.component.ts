15422dff44dc63f488030319c61144a0eb046bd3
RaymondCJA <raymondcja@gmail.com>
2019-12-22 23:59:58 +1000

    added form for checkout cart

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CartServiceService } from '../cart-service.service';
 
+import { FormBuilder } from '@angular/forms';
+
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -8,13 +10,28 @@ import { CartServiceService } from '../cart-service.service';
 })
 export class CartComponent {
   items;
-  
+  checkoutForm;
+
   constructor(
-    private cartService: CartServiceService
+    private cartService: CartServiceService,
+    private formBuilder: FormBuilder,
   ) { }
 
   ngOnInit() {
     this.items = this.cartService.getItems();
+
+    this.checkoutForm = this.formBuilder.group({
+      name: '',
+      address: ''
+    });
+  }
+
+  onSubmit(customerData) {
+    // Process checkout data here
+    console.warn('Your order has been submitted', customerData);
+
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
   }
 
 }
